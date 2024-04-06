2dd3ed2bd7f4c5a1b95888058d472b06dfbdd341
Sadiq Salau <sadiqsalau888@gmail.com>
2024-04-06 06:12:00 +0100

    Post inputs to registration api

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,7 @@
 import * as yup from "yup";
 import React from "react";
+import clsx from "clsx";
+import { Controller, FormProvider, useForm } from "react-hook-form";
 import {
   IonBackButton,
   IonButton,
@@ -10,13 +12,15 @@ import {
   IonItem,
   IonList,
   IonPage,
+  IonSpinner,
   IonText,
   IonToolbar,
 } from "@ionic/react";
 import { Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
 import { yupResolver } from "@hookform/resolvers/yup";
 
+import api from "../lib/api";
 // Logo Image
 import logo from "../assets/paaumarket.svg";
 
@@ -25,20 +29,30 @@ const schema = yup
   .object({
     name: yup.string().trim().required(),
     email: yup.string().trim().email().required(),
-    mobile_phone: yup.number().positive().required(),
+    mobile_number: yup.number().positive().required().label("Mobile Number"),
     password: yup.string().trim().max(16).required(),
   })
   .required();
 
 const Register: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
+  const form = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      name: "",
+      email: "",
+      mobile_number: undefined,
+      password: "",
+    },
+  });
 
-  const onSubmit = (data: object) => {
-    console.log(data);
+  const registrationMutation = useMutation({
+    mutationKey: ["register"],
+    mutationFn: (data) =>
+      api.post("/register", data).then((response) => response.data),
+  });
+
+  const onSubmit = (data: any) => {
+    registrationMutation.mutate(data);
   };
 
   return (
@@ -64,47 +78,106 @@ const Register: React.FC = () => {
             </h2>
           </IonText>
         </div>
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <IonList className="ion-padding ion-margin-bottom">
+              <Controller
+                name={"name"}
+                render={({ field, fieldState }) => (
+                  <IonItem>
+                    <IonInput
+                      type="text"
+                      label="Full Name"
+                      labelPlacement="floating"
+                      placeholder="Enter your name"
+                      name={field.name}
+                      onIonInput={field.onChange}
+                      onIonBlur={field.onBlur}
+                      value={field.value}
+                      errorText={fieldState.error?.message}
+                      className={clsx(
+                        fieldState.invalid && "ion-invalid ion-touched"
+                      )}
+                    ></IonInput>
+                  </IonItem>
+                )}
+              />
 
-        <IonList className="ion-padding">
-          <IonItem>
-            <IonInput
-              type="text"
-              label="Full Name"
-              labelPlacement="floating"
-              placeholder="Enter your name"
-            ></IonInput>
-          </IonItem>
-          <IonItem>
-            <IonInput
-              type="email"
-              label="Email"
-              labelPlacement="floating"
-              placeholder="Enter your Email address"
-            ></IonInput>
-          </IonItem>
+              <Controller
+                name={"email"}
+                render={({ field, fieldState }) => (
+                  <IonItem>
+                    <IonInput
+                      type="email"
+                      label="Email"
+                      labelPlacement="floating"
+                      placeholder="Enter your Email address"
+                      name={field.name}
+                      onIonInput={field.onChange}
+                      onIonBlur={field.onBlur}
+                      value={field.value}
+                      errorText={fieldState.error?.message}
+                      className={clsx(
+                        fieldState.invalid && "ion-invalid ion-touched"
+                      )}
+                    ></IonInput>
+                  </IonItem>
+                )}
+              />
 
-          <IonItem>
-            <IonInput
-              type="number"
-              label="Phone Number"
-              labelPlacement="floating"
-              placeholder="Enter your Phone Number"
-            ></IonInput>
-          </IonItem>
+              <Controller
+                name={"mobile_number"}
+                render={({ field, fieldState }) => (
+                  <IonItem>
+                    <IonInput
+                      type="number"
+                      label="Phone Number"
+                      labelPlacement="floating"
+                      placeholder="Enter your Phone Number"
+                      name={field.name}
+                      onIonInput={field.onChange}
+                      onIonBlur={field.onBlur}
+                      value={field.value}
+                      errorText={fieldState.error?.message}
+                      className={clsx(
+                        fieldState.invalid && "ion-invalid ion-touched"
+                      )}
+                    ></IonInput>
+                  </IonItem>
+                )}
+              />
 
-          <IonItem>
-            <IonInput
-              type="password"
-              label="Password"
-              labelPlacement="floating"
-              placeholder="Enter your password"
-            ></IonInput>
-          </IonItem>
-
-          <IonButton expand="full" shape="round" color="success">
-            Get Started
-          </IonButton>
-        </IonList>
+              <Controller
+                name={"password"}
+                render={({ field, fieldState }) => (
+                  <IonItem>
+                    <IonInput
+                      type="password"
+                      label="Password"
+                      labelPlacement="floating"
+                      placeholder="Enter your password"
+                      name={field.name}
+                      onIonInput={field.onChange}
+                      onIonBlur={field.onBlur}
+                      value={field.value}
+                      errorText={fieldState.error?.message}
+                      className={clsx(
+                        fieldState.invalid && "ion-invalid ion-touched"
+                      )}
+                    ></IonInput>
+                  </IonItem>
+                )}
+              />
+            </IonList>
+            <IonButton expand="full" shape="round" type="submit">
+              {registrationMutation.isPending ? (
+                <IonSpinner />
+              ) : (
+                <>Get Started</>
+              )}
+            </IonButton>
+          </form>
+        </FormProvider>
 
         <IonText className="ion-padding">
           <p className="text-center text-xs mb-5">
