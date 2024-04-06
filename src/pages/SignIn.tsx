4d81d3e09c91409ae51c88c242aa02f2d4fad00f
Abel Emmanuel <mannydev02@gmail.com>
2024-04-06 22:39:32 +0100

    Added Form validation

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -10,35 +10,45 @@ import {
   IonItem,
   IonList,
   IonPage,
+  IonSpinner,
   IonText,
   IonToolbar,
 } from "@ionic/react";
 import { Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import { Controller, FormProvider, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 // Logo Image
 import logo from "../assets/paaumarket.svg";
+import { useMutation } from "@tanstack/react-query";
+import api from "../lib/api";
+import clsx from "clsx";
 
 // Schema for form validation
 const schema = yup
   .object({
-    name: yup.string().trim().required(),
-    email: yup.string().trim().email().required(),
-    mobile_phone: yup.number().positive().required(),
-    password: yup.string().trim().max(16).required(),
+    email: yup.string().trim().email().required().label("Email"),
+    password: yup.string().trim().max(16).required().label("Password"),
   })
   .required();
 
 const SignIn: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
+  const form = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-  const onSubmit = (data: object) => {
-    console.log(data);
+  const logInMutation = useMutation({
+    mutationKey: ["login"],
+    mutationFn: (data) =>
+      api.post("/login", data).then((response) => response.data),
+  });
+
+  const onLogIn = (data: any) => {
+    logInMutation.mutate(data);
   };
 
   return (
@@ -65,29 +75,63 @@ const SignIn: React.FC = () => {
           </IonText>
         </div>
 
-        <IonList className="ion-padding">
-          <IonItem>
-            <IonInput
-              type="email"
-              label="Email"
-              labelPlacement="floating"
-              placeholder="Enter your Email address"
-            ></IonInput>
-          </IonItem>
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(onLogIn)}>
+            <IonList className="ion-padding ion-margin-bottom">
+              <Controller
+                name={"email"}
+                render={({ field, fieldState }) => (
+                  <IonItem>
+                    <IonInput
+                      type="email"
+                      label="Email"
+                      labelPlacement="floating"
+                      placeholder="Enter your email address"
+                      name={field.name}
+                      onIonInput={field.onChange}
+                      onIonBlur={field.onBlur}
+                      value={field.value}
+                      errorText={fieldState.error?.message}
+                      className={clsx(
+                        fieldState.invalid && "ion-invalid ion-touched"
+                      )}
+                    ></IonInput>
+                  </IonItem>
+                )}
+              />
 
-          <IonItem>
-            <IonInput
-              type="password"
-              label="Password"
-              labelPlacement="floating"
-              placeholder="Enter your password"
-            ></IonInput>
-          </IonItem>
+              <Controller
+                name={"password"}
+                render={({ field, fieldState }) => (
+                  <IonItem>
+                    <IonInput
+                      type="password"
+                      label="Password"
+                      labelPlacement="floating"
+                      placeholder="Enter your password"
+                      name={field.name}
+                      onIonInput={field.onChange}
+                      onIonBlur={field.onBlur}
+                      value={field.value}
+                      errorText={fieldState.error?.message}
+                      className={clsx(
+                        fieldState.invalid && "ion-invalid ion-touched"
+                      )}
+                    ></IonInput>
+                  </IonItem>
+                )}
+              />
 
-          <IonButton expand="full" shape="round" color="success">
-            Sign In
-          </IonButton>
-        </IonList>
+              <Link className="text-right block text-sm" to="/forget_password">
+                Forget Passowrd?
+              </Link>
+            </IonList>
+
+            <IonButton expand="full" shape="round" type="submit">
+              {logInMutation.isPending ? <IonSpinner /> : <>Sign In</>}
+            </IonButton>
+          </form>
+        </FormProvider>
 
         <IonText className="ion-text-center">
           <p>
