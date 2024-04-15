f63aa473f1055c6d308ca153c2dd768148824551
Abel Emmanuel <mannydev02@gmail.com>
2024-04-15 16:48:43 +0100

    Added Product

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import {
   IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
   IonContent,
+  IonGrid,
+  IonIcon,
   IonPage,
+  IonRow,
   IonSearchbar,
   IonText,
 } from "@ionic/react";
 import Header from "../component/Header";
+import { callOutline } from "ionicons/icons";
 
 export default function Home() {
   return (
@@ -21,15 +31,56 @@ export default function Home() {
       ></IonSearchbar>
 
       <IonContent className="ion-padding">
-        <p>
-          <IonButton routerLink="/login" expand="block">
-            Login
-          </IonButton>
-          <IonButton routerLink="/register" expand="block" color="dark">
-            Register
-          </IonButton>
-        </p>
+        <>
+          <b>Phone & Tablets</b>
+          <IonGrid>
+            <IonRow>
+              <Product
+                source="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/V/U/118566_1709127739.jpg"
+                product_name="Xiaomi Redmi A3 - 6.71 -4gb Ram/ 128gb"
+                product_price="N116,000.00"
+              />
+              <Product
+                source="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/C/S/225561_1699955276.jpg"
+                product_name="Apple iPhone 15 Pro 128GB White"
+                product_price="N1,532,600.00"
+              />
+
+              <Product
+                source="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/C/S/225561_1699955276.jpg"
+                product_name="Apple iPhone 15 Pro 128GB White"
+                product_price="N1,532,600.00"
+              />
+
+              <Product
+                source="https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/V/U/118566_1709127739.jpg"
+                product_name="Xiaomi Redmi A3 - 6.71 -4gb Ram/ 128gb"
+                product_price="N116,000.00"
+              />
+            </IonRow>
+          </IonGrid>
+        </>
       </IonContent>
     </IonPage>
   );
 }
+
+const Product = (props: any) => {
+  return (
+    <IonCol size="6" size-md="4" size-lg="2">
+      <IonCard>
+        <img alt={props.product_name} src={props.source} />
+        <IonCardHeader>
+          <IonCardTitle>{props.product_name}</IonCardTitle>
+          <IonCardSubtitle>{props.product_price}</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          <IonButton routerLink="wa.me/+2349025534431" expand="block">
+            <IonIcon icon={callOutline}></IonIcon>
+          </IonButton>
+        </IonCardContent>
+      </IonCard>
+    </IonCol>
+  );
+};
