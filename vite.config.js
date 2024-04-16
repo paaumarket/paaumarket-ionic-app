bbfb123360600304b047287834ef0da11c34b907
Sadiq Salau <sadiqsalau888@gmail.com>
2024-04-17 00:07:22 +0100

    Aliased src directory

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
+import { fileURLToPath, URL } from "url";
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   plugins: [react(), legacy()],
   test: {
     globals: true,
