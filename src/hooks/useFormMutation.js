f3fdf569effa278fb4dc9a12c008310e83e52e3d
Sadiq Salau <sadiqsalau888@gmail.com>
2024-04-17 00:19:09 +0100

    Refactored setting form error

--- src/hooks/useFormMutation.js
+++ src/hooks/useFormMutation.js
@@ -2,27 +2,27 @@ import { useIonToast } from "@ionic/react";
 import { useMutation } from "@tanstack/react-query";
 
 const useFormMutation = ({ form, ...props }) => {
-    const [toast, dismiss] = useIonToast();
-    return useMutation({
-        onMutate: () => dismiss(),
-        onError: (error) => {
-            if (error.response) {
-                const { message, errors } = error.response.data;
-                // Set form error
-                Object.entries(errors).forEach(([k, v]) =>
-                    form.setError(k, { message: v })
-                );
+  const [toast, dismiss] = useIonToast();
+  return useMutation({
+    onMutate: () => dismiss(),
+    onError: (error) => {
+      if (error.response) {
+        const { message, errors } = error.response.data;
+        // Set form error
+        for (const k in errors) {
+          form.setError(k, { message: errors[k] });
+        }
 
-                // Toast
-                toast({
-                    message,
-                    duration: 3000,
-                    color: "danger",
-                });
-            }
-        },
-        ...props,
-    });
+        // Toast
+        toast({
+          message,
+          duration: 3000,
+          color: "danger",
+        });
+      }
+    },
+    ...props,
+  });
 };
 
 export default useFormMutation;
