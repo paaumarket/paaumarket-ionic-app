8494850c514607c24399c386b7efc68d8362b419
Abel Emmanuel <mannydev02@gmail.com>
2024-04-19 17:35:51 +0100

    Added Category Page

---
+++ src/pages/Category.jsx
@@ -0,0 +1,44 @@
+import {
+  IonBackButton,
+  IonButtons,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonPage,
+  IonRow,
+} from "@ionic/react";
+import Header from "../component/Header";
+import { useParams } from "react-router";
+import api from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import Advert, { AdvertPlaceholder } from "@/component/Advert";
+
+export default function Category() {
+  const { id } = useParams();
+  const { isPending, data } = useQuery({
+    queryKey: ["advert", id],
+    queryFn: ({ signal }) =>
+      api.get(`/adverts`, { signal }).then((response) => response.data),
+  });
+
+  return (
+    <IonPage>
+      <Header>
+        <IonButtons slot="start">
+          <IonBackButton defaultHref="/"></IonBackButton>
+        </IonButtons>
+        <div className="grow">Category Name</div>
+      </Header>
+
+      <IonContent className="ion-padding">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              {isPending ? <AdvertPlaceholder /> : <Advert advert={data} />}
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+}
