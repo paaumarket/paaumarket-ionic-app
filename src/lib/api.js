87fb4d0d98a2f7b622017c2e2ec6342be8b7f981
Sadiq Salau <sadiqsalau888@gmail.com>
2024-04-20 09:09:16 +0100

    Set authorization token on requests

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,21 @@
+import { selectToken } from "@/app/features/auth/authSlice";
+import { store } from "@/app/store";
 import axios from "axios";
 
-export default axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
+api.interceptors.request.use((config) => {
+  /** Retrieve authorization token */
+  const token = selectToken(store.getState());
+
+  if (token) {
+    /** Set authorization header */
+    config.headers.setAuthorization(`Bearer ${token}`);
+  }
+
+  return config;
+});
+
+export default api;
