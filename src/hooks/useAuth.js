a03a87a5161578d319b9aced6a66d0057dd4197e
Sadiq Salau <sadiqsalau888@gmail.com>
2024-04-20 09:24:05 +0100

    Select and return permissions

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,18 +1,24 @@
-import { login, logout, selectUser } from "@/app/features/auth/authSlice";
+import {
+  login,
+  logout,
+  selectPermissions,
+  selectUser,
+} from "@/app/features/auth/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function useAuth() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    return {
-        user: useSelector(selectUser),
+  return {
+    user: useSelector(selectUser),
+    permissions: useSelector(selectPermissions),
 
-        // Methods
-        login: (data) => {
-            return dispatch(login(data));
-        },
-        logout: () => {
-            return dispatch(logout());
-        },
-    };
+    // Methods
+    login: (data) => {
+      return dispatch(login(data));
+    },
+    logout: () => {
+      return dispatch(logout());
+    },
+  };
 }
