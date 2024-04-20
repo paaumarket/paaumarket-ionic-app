8c2f8f236f6d487587e840c11cc7ea35d8ba100b
Sadiq Salau <sadiqsalau888@gmail.com>
2024-04-20 09:20:20 +0100

    Added permissions to authSlice

--- src/app/features/auth/authSlice.js
+++ src/app/features/auth/authSlice.js
@@ -2,24 +2,27 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // Initial State
 const initialState = {
-    user: null,
-    token: null,
+  user: null,
+  token: null,
+  permissions: [],
 };
 
 // Slice
 export const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        login(state, { payload }) {
-            state.user = payload.user || state.user;
-            state.token = payload.token || state.token;
-        },
-        logout(state) {
-            state.user = null;
-            state.token = null;
-        },
+  name: "auth",
+  initialState,
+  reducers: {
+    login(state, { payload }) {
+      state.user = payload.user || state.user;
+      state.token = payload.token || state.token;
+      state.permissions = payload.permissions || state.permissions;
     },
+    logout(state) {
+      state.user = initialState.user;
+      state.token = initialState.token;
+      state.permissions = initialState.permissions;
+    },
+  },
 });
 
 // Actions
