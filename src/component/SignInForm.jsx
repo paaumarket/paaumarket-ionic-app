d578735688f6e139375366641728005021d9c4ea
Sadiq Salau <sadiqsalau888@gmail.com>
2024-04-25 04:47:06 +0100

    Extracted signin form into its component

---
+++ src/component/SignInForm.jsx
@@ -0,0 +1,126 @@
+import * as yup from "yup";
+import {
+  IonButton,
+  IonInput,
+  IonInputPasswordToggle,
+  IonItem,
+  IonList,
+  IonSpinner,
+  IonText,
+} from "@ionic/react";
+import { Link } from "react-router-dom";
+import { Controller, FormProvider, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+// Logo Image
+import api from "../lib/api";
+import clsx from "clsx";
+import useAuth from "@/hooks/useAuth";
+import useFormMutation from "@/hooks/useFormMutation";
+
+// Schema for form validation
+const schema = yup
+  .object({
+    email: yup.string().trim().email().required().label("Email"),
+    password: yup.string().trim().max(16).required().label("Password"),
+  })
+  .required();
+
+const SignInForm = ({ onSuccess }) => {
+  const { login } = useAuth();
+
+  const form = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const logInMutation = useFormMutation({
+    form,
+    mutationKey: ["login"],
+    mutationFn: (data) =>
+      api.post("/login", data).then((response) => response.data),
+  });
+
+  const onLogIn = (data) => {
+    logInMutation.mutate(data, {
+      onSuccess(auth) {
+        login(auth);
+
+        onSuccess();
+      },
+    });
+  };
+
+  return (
+    <FormProvider {...form}>
+      <form onSubmit={form.handleSubmit(onLogIn)}>
+        <IonList className="ion-padding ion-margin-bottom">
+          <Controller
+            name={"email"}
+            render={({ field, fieldState }) => (
+              <IonItem>
+                <IonInput
+                  type="email"
+                  label="Email"
+                  labelPlacement="stacked"
+                  placeholder="Enter your email address"
+                  name={field.name}
+                  onIonInput={field.onChange}
+                  onIonBlur={field.onBlur}
+                  value={field.value}
+                  errorText={fieldState.error?.message}
+                  className={clsx(
+                    fieldState.invalid && "ion-invalid ion-touched"
+                  )}
+                ></IonInput>
+              </IonItem>
+            )}
+          />
+
+          <Controller
+            name={"password"}
+            render={({ field, fieldState }) => (
+              <IonItem>
+                <IonInput
+                  type="password"
+                  label="Password"
+                  labelPlacement="stacked"
+                  placeholder="Enter your password"
+                  name={field.name}
+                  onIonInput={field.onChange}
+                  onIonBlur={field.onBlur}
+                  value={field.value}
+                  errorText={fieldState.error?.message}
+                  className={clsx(
+                    fieldState.invalid && "ion-invalid ion-touched"
+                  )}
+                >
+                  <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
+                </IonInput>
+              </IonItem>
+            )}
+          />
+
+          <Link className="block text-sm text-right" to="/forget_password">
+            Forget Password?
+          </Link>
+        </IonList>
+
+        <IonButton expand="full" shape="round" type="submit">
+          {logInMutation.isPending ? <IonSpinner /> : <>Sign In</>}
+        </IonButton>
+
+        <IonText className="ion-text-center">
+          <p>
+            Create an account? <Link to="/register">Sign Up</Link>
+          </p>
+        </IonText>
+      </form>
+    </FormProvider>
+  );
+};
+
+export default SignInForm;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,65 +1,18 @@
-import * as yup from "yup";
 import {
   IonBackButton,
-  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
-  IonInput,
-  IonItem,
-  IonList,
   IonPage,
-  IonSpinner,
   IonText,
   IonToolbar,
 } from "@ionic/react";
-import { Link } from "react-router-dom";
-import { Controller, FormProvider, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
 
 // Logo Image
 import logo from "../assets/paaumarket.svg";
-import api from "../lib/api";
-import clsx from "clsx";
-import useAuth from "@/hooks/useAuth";
-import useFormMutation from "@/hooks/useFormMutation";
-
-// Schema for form validation
-const schema = yup
-  .object({
-    email: yup.string().trim().email().required().label("Email"),
-    password: yup.string().trim().max(16).required().label("Password"),
-  })
-  .required();
+import SignInForm from "@/component/SignInForm";
 
 const SignIn = ({ history }) => {
-  const { login } = useAuth();
-
-  const form = useForm({
-    resolver: yupResolver(schema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const logInMutation = useFormMutation({
-    form,
-    mutationKey: ["login"],
-    mutationFn: (data) =>
-      api.post("/login", data).then((response) => response.data),
-  });
-
-  const onLogIn = (data) => {
-    logInMutation.mutate(data, {
-      onSuccess(auth) {
-        login(auth);
-
-        history.replace("/home");
-      },
-    });
-  };
-
   return (
     <IonPage>
       <IonHeader className="shadow-none">
@@ -84,69 +37,7 @@ const SignIn = ({ history }) => {
           </IonText>
         </div>
 
-        <FormProvider {...form}>
-          <form onSubmit={form.handleSubmit(onLogIn)}>
-            <IonList className="ion-padding ion-margin-bottom">
-              <Controller
-                name={"email"}
-                render={({ field, fieldState }) => (
-                  <IonItem>
-                    <IonInput
-                      type="email"
-                      label="Email"
-                      labelPlacement="floating"
-                      placeholder="Enter your email address"
-                      name={field.name}
-                      onIonInput={field.onChange}
-                      onIonBlur={field.onBlur}
-                      value={field.value}
-                      errorText={fieldState.error?.message}
-                      className={clsx(
-                        fieldState.invalid && "ion-invalid ion-touched"
-                      )}
-                    ></IonInput>
-                  </IonItem>
-                )}
-              />
-
-              <Controller
-                name={"password"}
-                render={({ field, fieldState }) => (
-                  <IonItem>
-                    <IonInput
-                      type="password"
-                      label="Password"
-                      labelPlacement="floating"
-                      placeholder="Enter your password"
-                      name={field.name}
-                      onIonInput={field.onChange}
-                      onIonBlur={field.onBlur}
-                      value={field.value}
-                      errorText={fieldState.error?.message}
-                      className={clsx(
-                        fieldState.invalid && "ion-invalid ion-touched"
-                      )}
-                    ></IonInput>
-                  </IonItem>
-                )}
-              />
-
-              <Link className="block text-sm text-right" to="/forget_password">
-                Forget Passowrd?
-              </Link>
-            </IonList>
-
-            <IonButton expand="full" shape="round" type="submit">
-              {logInMutation.isPending ? <IonSpinner /> : <>Sign In</>}
-            </IonButton>
-          </form>
-        </FormProvider>
-
-        <IonText className="ion-text-center">
-          <p>
-            Create an account? <Link to="/register">Sign Up</Link>
-          </p>
-        </IonText>
+        <SignInForm onSuccess={() => history.replace("/home")} />
       </IonContent>
     </IonPage>
   );
