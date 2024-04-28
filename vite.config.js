3b651f63cf7b85e12ae7e76c5d2a8de66bd0e06c
Sadiq Salau <sadiqsalau888@gmail.com>
2024-04-28 16:00:42 +0100

    Added PWA Support

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "url";
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,42 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  plugins: [react(), legacy()],
+  plugins: [
+    react(),
+    legacy(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Paau Market",
+        short_name: "Paau Market",
+        description: "Buy & Sell Everything",
+        theme_color: "#4099ff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
