c6d7e185f0c22c71277ba60563b796a7e501123b
Abel Emmanuel <mannydev02@gmail.com>
2024-05-14 11:39:27 +0100

    Added Support Line Page

---
+++ src/pages/SupportLine.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+
+import {
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonItemGroup,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonPage,
+  IonRouterLink,
+  IonText,
+} from "@ionic/react";
+import {
+  callOutline,
+  logoFacebook,
+  logoTwitter,
+  logoWhatsapp,
+  mailOutline,
+} from "ionicons/icons";
+
+export default function SupportLine() {
+  return (
+    <IonPage>
+      <IonContent className="ion-padding" fullscreen>
+        <div className="mt-10"></div>
+        <IonText>
+          <div className="text-center">
+            <IonIcon size="large" icon={callOutline}></IonIcon>
+            <h2 className="text-center">
+              <b>Get in touch with us for more information</b>
+            </h2>
+          </div>
+        </IonText>
+
+        {/* <div className="mt-5">
+          <p> </p>
+        </div> */}
+
+        <IonList>
+          <IonListHeader>
+            <IonLabel>Support line case you need help.</IonLabel>
+          </IonListHeader>
+          <IonItemGroup>
+            <IonRouterLink href="tel:+2349025534431">
+              <IonItem>
+                <IonIcon slot="start" color={"primary"} icon={callOutline} />
+                <IonLabel>+234 90 255 344 31</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+
+            <IonRouterLink href="mailto:support@paaumarket.com.ng">
+              <IonItem>
+                <IonIcon slot="start" color={"primary"} icon={mailOutline} />
+                <IonLabel>support@paaumarket.com.ng</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+          </IonItemGroup>
+        </IonList>
+
+        <IonList>
+          <IonListHeader>
+            <IonLabel>Social Media</IonLabel>
+          </IonListHeader>
+          <IonItemGroup>
+            <IonRouterLink href="https://wa.me/+2349025534431">
+              <IonItem>
+                <IonIcon slot="start" color={"primary"} icon={logoWhatsapp} />
+                <IonLabel>Text Us on WhatsApp</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+
+            <IonRouterLink href="https://web.facebook.com/profile.php?id=61559206804023">
+              <IonItem>
+                <IonIcon slot="start" color={"primary"} icon={logoFacebook} />
+                <IonLabel>Follow Us on Facebook</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+
+            <IonRouterLink href="https://x.com/paaumarket">
+              <IonItem>
+                <IonIcon slot="start" color={"primary"} icon={logoTwitter} />
+                <IonLabel>Follow Us on Twitter</IonLabel>
+              </IonItem>
+            </IonRouterLink>
+          </IonItemGroup>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+}
