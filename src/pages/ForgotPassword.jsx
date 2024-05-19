939d0a8a0d255c519f7bd83956fc7349b8f96282
Sadiq Salau <sadiqsalau888@gmail.com>
2024-05-19 08:32:39 +0100

    Implemented password reset

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -11,6 +11,7 @@ import {
   IonText,
   IonTitle,
   IonToolbar,
+  useIonToast,
 } from "@ionic/react";
 
 import { useForm } from "react-hook-form";
@@ -20,43 +21,14 @@ import useFormMutation from "@/hooks/useFormMutation";
 import api from "@/lib/api";
 import { useOTPVerification } from "@/components/OTPVerification/useOTPVerification";
 import FormIonInput from "@/components/FormIonInput";
-
-// Schema for form validation
-const schema = yup
-  .object({
-    email: yup.string().trim().email().required().label("Email"),
-  })
-  .required();
+import { useState } from "react";
+import { useMemo } from "react";
+import PasswordIonInput from "@/components/PasswordIonInput";
+import { useHistory } from "react-router";
 
 export default function ForgotPassword() {
-  const form = useForm({
-    resolver: yupResolver(schema),
-    defaultValues: {
-      email: "",
-    },
-  });
-
-  const passwordMutation = useFormMutation({
-    form,
-    mutationKey: ["forgot-password"],
-    mutationFn: (data) =>
-      api.post("/forgot-password", data).then((response) => response.data),
-  });
-
-  const onSubmit = (data) => {
-    passwordMutation.mutate(data, {
-      onSuccess(data) {
-        showOTP(data["email"]);
-      },
-    });
-  };
-
-  const [showOTP, closeOtp] = useOTPVerification({
-    onSuccess() {
-      closeOtp();
-      //
-    },
-  });
+  const history = useHistory();
+  const [email, setEmail] = useState(null);
 
   return (
     <IonPage>
@@ -71,43 +43,183 @@ export default function ForgotPassword() {
 
       <IonContent className="ion-padding" fullscreen>
         <div className="flex flex-col gap-4">
-          <h2>
-            <b>Forget Password?</b>
-          </h2>
-
-          <p>
-            <IonText color="medium">
-              Don't worry! It occurs. Please enter the email address linked with
-              your account.
-            </IonText>
-          </p>
-
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <IonList>
-              {/* Email */}
-              <IonItem>
-                <FormIonInput
-                  {...form.register("email")}
-                  label="Your email"
-                  labelPlacement="floating"
-                  placeholder="Enter your email"
-                  errorText={form.formState.errors["email"]?.message}
-                />
-              </IonItem>
-            </IonList>
-
-            <IonButton
-              disabled={passwordMutation.isPending}
-              expand="full"
-              shape="round"
-              type="submit"
-              className="ion-margin-top"
-            >
-              {passwordMutation.isPending ? <IonSpinner /> : <>Send Code</>}
-            </IonButton>
-          </form>
+          {email ? (
+            <ResetPasswordForm
+              email={email}
+              onSuccess={() => history.replace("/login")}
+            />
+          ) : (
+            <ForgotPasswordForm onSuccess={(data) => setEmail(data["email"])} />
+          )}
         </div>
       </IonContent>
     </IonPage>
   );
 }
+
+const ForgotPasswordForm = ({ onSuccess }) => {
+  // Schema for form validation
+  const schema = useMemo(
+    () =>
+      yup
+        .object({
+          email: yup.string().trim().email().required().label("Email"),
+        })
+        .required(),
+    []
+  );
+
+  const form = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const formMutation = useFormMutation({
+    form,
+    mutationKey: ["forgot-password"],
+    mutationFn: (data) =>
+      api.post("/forgot-password", data).then((response) => response.data),
+  });
+
+  const onSubmit = (data) => {
+    formMutation.mutate(data, {
+      onSuccess(data) {
+        showOTP(data["email"]);
+      },
+    });
+  };
+
+  const [showOTP, closeOtp] = useOTPVerification({
+    onSuccess(data) {
+      closeOtp();
+      onSuccess(data);
+    },
+  });
+
+  return (
+    <>
+      <h2>
+        <b>Forget Password?</b>
+      </h2>
+
+      <p>
+        <IonText color="medium">
+          Don't worry! It occurs. Please enter the email address linked with
+          your account.
+        </IonText>
+      </p>
+
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <IonList>
+          {/* Email */}
+          <IonItem>
+            <FormIonInput
+              {...form.register("email")}
+              label="Your email"
+              labelPlacement="floating"
+              placeholder="Enter your email"
+              errorText={form.formState.errors["email"]?.message}
+              disabled={formMutation.isPending}
+            />
+          </IonItem>
+        </IonList>
+
+        <IonButton
+          disabled={formMutation.isPending}
+          expand="full"
+          shape="round"
+          type="submit"
+          className="ion-margin-top"
+        >
+          {formMutation.isPending ? <IonSpinner /> : <>Send Code</>}
+        </IonButton>
+      </form>
+    </>
+  );
+};
+
+const ResetPasswordForm = ({ email, onSuccess }) => {
+  const [presentToast] = useIonToast();
+  // Schema for form validation
+  const schema = useMemo(
+    () =>
+      yup
+        .object({
+          password: yup.string().trim().max(16).required().label("Password"),
+        })
+        .required(),
+    []
+  );
+
+  const form = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      password: "",
+    },
+  });
+
+  const formMutation = useFormMutation({
+    form,
+    mutationKey: ["reset-password"],
+    mutationFn: (data) =>
+      api.post("/reset-password", data).then((response) => response.data),
+    onSuccess() {
+      presentToast({
+        message: "Password was successfully updated!",
+        color: "success",
+        duration: 2000,
+      });
+    },
+  });
+
+  const onSubmit = (data) => {
+    formMutation.mutate(
+      {
+        email,
+        ...data,
+      },
+      {
+        onSuccess,
+      }
+    );
+  };
+
+  return (
+    <>
+      <h2 className="m-0 font-bold">Reset Password</h2>
+
+      <p>
+        <IonText color="medium">
+          Enter a new password for your account with the email address ({email}
+          ).
+        </IonText>
+      </p>
+
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <IonList>
+          {/* Password */}
+          <IonItem>
+            <PasswordIonInput
+              {...form.register("password")}
+              label="Password"
+              errorText={form.formState.errors["password"]?.message}
+              disabled={formMutation.isPendings}
+            />
+          </IonItem>
+        </IonList>
+
+        <IonButton
+          disabled={formMutation.isPending}
+          expand="full"
+          shape="round"
+          type="submit"
+          className="ion-margin-top"
+        >
+          {formMutation.isPending ? <IonSpinner /> : <>Update Password</>}
+        </IonButton>
+      </form>
+    </>
+  );
+};
