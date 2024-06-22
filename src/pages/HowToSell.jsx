edd17b2d1a4e95f76ecfdea97beaa5039383ced0
Abel Emmanuel <mannydev02@gmail.com>
2024-06-22 20:46:37 +0100

    How To Sell

---
+++ src/pages/HowToSell.jsx
@@ -0,0 +1,57 @@
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonItemGroup,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonPage,
+  IonRouterLink,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import {
+  callOutline,
+  logoFacebook,
+  logoTwitter,
+  logoWhatsapp,
+  mailOutline,
+} from "ionicons/icons";
+
+export default function HowToSell() {
+  return (
+    <IonPage>
+      <IonHeader className="shadow-none">
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/app"></IonBackButton>
+          </IonButtons>
+          <IonTitle>How To Sell</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent className="ion-padding" fullscreen>
+        <div className="mt-10"></div>
+        <IonText>
+          <div>
+            <h2 className="text-center">
+              <b>WATCH NOW: How To Sell.</b>
+            </h2>
+          </div>
+        </IonText>
+
+        <div>
+          <video controls className="w-full">
+
+          <source src="https://youtu.be/I_l4QIR5D8Y"  />
+          </video>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+}
