05460f22001b8974b1d79b1a0363717e61dd0521
Abel Emmanuel <mannydev02@gmail.com>
2024-06-23 18:42:15 +0100

    Change sell iframe element to video element

--- src/pages/HowToSell.jsx
+++ src/pages/HowToSell.jsx
@@ -3,25 +3,12 @@ import {
   IonButtons,
   IonContent,
   IonHeader,
-  IonIcon,
-  IonItem,
-  IonItemGroup,
-  IonLabel,
-  IonList,
-  IonListHeader,
   IonPage,
-  IonRouterLink,
   IonText,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import {
-  callOutline,
-  logoFacebook,
-  logoTwitter,
-  logoWhatsapp,
-  mailOutline,
-} from "ionicons/icons";
+
 
 export default function HowToSell() {
   return (
@@ -45,8 +32,13 @@ export default function HowToSell() {
           </div>
         </IonText>
 
-        <div>
-          <iframe className="w-full aspect-video" src="https://www.youtube.com/embed/I_l4QIR5D8Y?si=Y2IGG1OUbYw-P_xk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <div className="flex justify-center">
+          <video controls width="400">
+            <source src="/how_to_sell.mp4" />
+          </video>
+
+          {/* YouTube */}
+          {/* <iframe className="w-full aspect-video" src="https://www.youtube.com/embed/I_l4QIR5D8Y?si=Y2IGG1OUbYw-P_xk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> */}
         </div>
       </IonContent>
     </IonPage>
