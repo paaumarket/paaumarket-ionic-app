27a7ae166a38e1db33460453635a31b278bdbb01
Abel Emmanuel <mannydev02@gmail.com>
2024-08-10 16:28:53 +0100

    Added Login and Sign Up

--- src/pages/SingleAdvertPage.jsx
+++ src/pages/SingleAdvertPage.jsx
@@ -18,16 +18,27 @@ import {
   IonIcon,
   IonPage,
   IonRow,
-  IonTitle,
   IonToolbar,
   useIonLoading,
+  IonModal,
+  IonText,
+  IonTitle,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
 } from "@ionic/react";
-import { Link } from "react-router-dom";
 import { callOutline, logoWhatsapp, warningOutline } from "ionicons/icons";
 import { useMutation } from "@tanstack/react-query";
 import { useLocation, useParams } from "react-router-dom";
 import withIonPageQueryRefetch from "@/hoc/withIonPageQueryRefetch";
 import { useApiInfiniteQuery, useApiQuery } from "@/hooks/useApiQuery";
+
+import logo from "@/assets/paaumarket.svg";
+import SignInForm from "@/components/SignInForm";
+import { useHistory } from "react-router-dom";
+import { useRef } from "react";
+import { useState } from "react";
+import RegisterForm from "@/components/RegisterForm";
 
 export default withIonPageQueryRefetch(function SingleAdvertPage() {
   const { user } = useAuth();
@@ -83,12 +94,16 @@ export default withIonPageQueryRefetch(function SingleAdvertPage() {
           ) : null}
         </IonGrid>
       </IonContent>
+      {/* <SignInModal /> */}
     </IonPage>
   );
 });
 
 const AdvertContact = ({ advert }) => {
+  const modal = useRef(null);
   const location = useLocation();
+  const [segment, setSegment] = useState("login");
+
   const { user } = useAuth();
   const { data, mutateAsync, isSuccess } = useMutation({
     mutationKey: ["advert", advert["id"], "contact"],
@@ -126,7 +141,95 @@ const AdvertContact = ({ advert }) => {
           {!user ? (
             <p>
               You need to sign in first to see contact:{" "}
-              <Link to={`/login?return=${location.pathname}`}>Login</Link>
+              <IonButton
+                expand="full"
+                shape="round"
+                fill="solid"
+                color="primary"
+                id="open-modal"
+              >
+                <IonIcon
+                  icon={callOutline}
+                  className="ion-padding-end"
+                ></IonIcon>
+                Show Contact
+              </IonButton>
+              <IonModal ref={modal} trigger="open-modal">
+                <IonHeader className="shadow-none">
+                  <IonToolbar>
+                    <IonButtons slot="start">
+                      <IonButton onClick={() => modal.current?.dismiss()}>
+                        Cancel
+                      </IonButton>
+                    </IonButtons>
+                    <IonSegment
+                      value={segment}
+                      onIonChange={(ev) => setSegment(ev.detail.value)}
+                    >
+                      {/* LOGIN */}
+                      <IonSegmentButton value="login">
+                        <IonLabel>Login</IonLabel>
+                      </IonSegmentButton>
+                      {/* REGISTER */}
+                      <IonSegmentButton value="signup">
+                        <IonLabel>Sign Up</IonLabel>
+                      </IonSegmentButton>
+                    </IonSegment>
+                  </IonToolbar>
+                </IonHeader>
+
+                <IonContent className="ion-padding">
+                  {segment === "login" ? (
+                    <>
+                      <div className="flex flex-col items-center justify-center gap-4 p-4">
+                        <img
+                          src={logo}
+                          alt="Paau Market Logo"
+                          className="w-20 h-20 mx-auto mt-10"
+                        />
+                        <IonText className="ion-text-color">
+                          <h2 className="font-light text-center ion-no-margin text-md">
+                            Welcome back to PAAU Market
+                          </h2>
+                        </IonText>
+                      </div>
+
+                      <SignInForm
+                        onSuccess={() => {
+                          modal.current?.dismiss();
+
+                          handleShowContact();
+                        }}
+                      />
+                    </>
+                  ) : null}
+
+                  {segment === "signup" ? (
+                    <>
+                      <div className="flex flex-col items-center justify-center gap-4 p-4">
+                        <img
+                          src={logo}
+                          alt="Paau Market Logo"
+                          className="w-20 h-20 mx-auto mt-10"
+                        />
+                        <IonText className="ion-text-color">
+                          <h2 className="font-light text-center ion-no-margin text-md">
+                            Sign up to PAAU Market
+                          </h2>
+                        </IonText>
+                      </div>
+
+                      <RegisterForm
+                        onSuccess={() => {
+                          modal.current?.dismiss();
+
+                          handleShowContact();
+                        }}
+                      />
+                    </>
+                  ) : null}
+                </IonContent>
+              </IonModal>
             </p>
           ) : isSuccess ? (
             <>
